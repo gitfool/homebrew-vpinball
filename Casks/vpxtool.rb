fd709a850d5f14baaed6809ebed4814dbb49b7b3
Sean Fausett <sean@jellyfish.co.nz>
2026-03-11 21:55:38 +1300

    Fix vpxtool artifacts
    https://github.com/francisdb/vpxtool/pull/704

--- Casks/vpxtool.rb
+++ Casks/vpxtool.rb
@@ -1,8 +1,6 @@
 cask "vpxtool" do
   arch arm: "aarch64", intel: "x86_64"
-  os macos: "macOS", linux: "Linux"
-
-  musl = on_system_conditional macos: "", linux: "-musl"
+  os macos: "macos", linux: "linux"
 
   version "0.26.1"
   sha256 arm:          "7aac2490a0091fb0a5b287dd74014a8eae75c51bf143593235bb09b8396ecbe8",
@@ -10,7 +8,7 @@ cask "vpxtool" do
          arm64_linux:  "ddb3bf46a562e81fab56dcd1aa613b692302cc6c2fb03fa51646a9006c3db085",
          x86_64_linux: "2fa2a869147e7edc7dd20615379594eece2074dfd3cc4616f34b12145f3dc526"
 
-  url "https://github.com/francisdb/vpxtool/releases/download/v#{version}/vpxtool-#{os}-#{arch}#{musl}-v#{version}.tar.gz"
+  url "https://github.com/francisdb/vpxtool/releases/download/v#{version}/vpxtool-#{os}-#{arch}-v#{version}.tar.gz"
   name "vpxtool"
   desc "Terminal based frontend and utilities for Visual Pinball"
   homepage "https://github.com/francisdb/vpxtool"
