32b0722a5156a08bbfcc5a09d0bfef288d4a1f6c
Sean Fausett <sean@jellyfish.co.nz>
2026-03-14 07:42:49 +1300

    Fix vpinball-nightly artifacts

--- Casks/vpinball-nightly.rb
+++ Casks/vpinball-nightly.rb
@@ -2,12 +2,14 @@ cask "vpinball-nightly" do
   arch arm: "arm64", intel: "x64"
   os macos: "macos", linux: "linux"
 
+  ext = on_system_conditional macos: "dmg", linux: "zip"
+
   version "10.8.1-4787-eb9f66e"
   sha256 arm:          "81f6d9f0855e94be6d2b58bda714ab94b890f75e03efd107485073de2e53d561",
          intel:        "1f9a7341ee3c43548c20822436bfdb6a75e3b0bc332d82d91e1fd6de55af67e9",
          x86_64_linux: "863b47e29bd976ef1c2d7431d67f0f281facee7959de32928b5950c22a5c650c"
 
-  url "https://nightly.link/vpinball/vpinball/workflows/vpinball/master/VPinballX_BGFX-#{version}-#{os}-#{arch}-Release.zip",
+  url "https://nightly.link/vpinball/vpinball/workflows/vpinball/master/VPinballX_BGFX-#{version}-#{os}-#{arch}-Release.#{ext}",
       verified: "nightly.link/vpinball/vpinball/"
   name "VPinballX BGFX (nightly)"
   desc "Visual Pinball BGFX (nightly)"
