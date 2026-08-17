9ab8fed46fa4deaf0f9ebc1e6933c46d9b5bf585
Sean Fausett <sean@jellyfish.co.nz>
2026-08-17 15:50:24 +1200

    Add vpx editor

---
+++ Casks/vpx-editor.rb
@@ -0,0 +1,52 @@
+cask "vpx-editor" do
+  arch arm: "arm64", intel: "x64"
+  os macos: "macos", linux: "linux"
+  ext = on_system_conditional macos: "dmg", linux: "zip"
+
+  version "0.8.32-125-9f699df"
+  sha256 arm:          "7cdb3736305d36a29795fe0b6668eb1b702493d2d728e21cffc445967c1bf90d",
+         x86_64_linux: "db1f6317750f4f3f87a130586d50a6feacc2a0ae71494fa62c535acbb6ee5b95"
+
+  on_macos do
+    depends_on arch: :arm64
+    depends_on macos: :monterey
+
+    app "VPX Editor.app"
+
+    postflight do
+      system_command "xattr", args: ["-d", "com.apple.quarantine", "#{appdir}/VPX Editor.app"]
+    end
+  end
+  on_linux do
+    depends_on arch: :x86_64
+
+    binary "vpx-editor"
+  end
+
+  url "https://github.com/jsm174/vpx-editor/releases/download/v#{version.split("-").first}/vpx-editor-#{os}-#{arch}-#{version}.#{ext}"
+  name "VPX Editor"
+  desc "Cross-platform Visual Pinball table editor using Electron and vpin"
+  homepage "https://github.com/jsm174/vpx-editor"
+
+  livecheck do
+    url :url
+    regex(/vpx-editor-#{os}-#{arch}-(\d+(?:\.\d+)+-\d+-[0-9a-f]{7})\.#{ext}/i)
+    strategy :github_releases do |json, regex|
+      json.map do |release|
+        next if release["draft"]
+
+        release["assets"]&.filter_map do |asset|
+          match = asset["name"]&.match(regex)
+          next if match.blank?
+
+          match[1]
+        end
+      end.flatten.compact.first
+    end
+  end
+
+  zap trash: [
+    "~/.config/vpx-editor",
+    "~/Library/Application Support/vpx-editor",
+  ]
+end
